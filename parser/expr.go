c0f32d867850008c19df6a95294710da7f067e93
Anita Anderson <anita@ansan.io>
2025-04-08 01:44:12 -0400

    Fix license

--- parser/expr.go
+++ parser/expr.go
@@ -1,6 +1,6 @@
 package parser
 
-// Copyright (C) 2024 Anita Anderson
+// Copyright (C) 2025 Anita Anderson
 
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
